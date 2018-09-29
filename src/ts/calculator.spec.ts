dda6829a7436e823891abc731f7cf617f09eebc5
Magnus Everö <magnus.evero@gmail.com>
2018-09-29 20:50:21 +0200

    All ways to calculate implemented

--- src/ts/calculator.spec.ts
+++ src/ts/calculator.spec.ts
@@ -2,11 +2,21 @@ import { calculator } from './calculator';
 import { expect } from 'chai';
 import 'mocha';
 
-
-describe('Calculator add', () => {
+describe('Miniräknare', () => {
   var calc = new calculator();
-  it('should return 7 when adding 2 and 5', () => {
-    const result = calc.add(2, 5);
-    expect(result).to.equal(7);
+  it('ska returnera 7 när man kör add på 2 och 5', () => {
+    expect(calc.add(2, 5)).to.equal(7);
+  });
+
+  it('ska returnera 3 när man tar 6 minus 3', () => {
+    expect(calc.subtract(6, 3)).to.equal(3);
+  });
+
+  it('ska returnera 12 när man tar 3 gånger 4', () => {
+    expect(calc.multiply(3, 4)).to.equal(12);
+  });
+
+  it('ska returnera 2 när man tar 4 delat med 2', () => {
+    expect(calc.divide(4, 2)).to.equal(2);
   });
 });
--- src/ts/calculator.ts
+++ src/ts/calculator.ts
@@ -1,5 +1,22 @@
 export class calculator {
+
     public add(a: number, b: number): number {
         return a + b;
     }
+
+    public subtract(a: number, b: number): number {
+        return a - b;
+    }
+
+    public multiply(a: number, b: number): number{
+        return a * b ;
+    }
+
+    public  divide(a:number, b: number):number{
+        return a / b ;
+    }
+
+
+//    divide
+
 }
